cc5b6cd4fb44cf8be64d26a2a8d46ea99b2e6579
mListek <michal.listex@gmail.com>
2023-03-27 18:30:37 +0200

    moved priceJson to a separate file

---
+++ src/main/java/PriceJson.java
@@ -0,0 +1,44 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+public class PriceJson implements Callable<String> {
+
+  private String url;
+  private String userAgent;
+
+  public PriceJson(String url, String userAgent) {
+    this.url = url;
+    this.userAgent = userAgent;
+  }
+  @Override
+  public String call() throws Exception {
+    try {
+      Document doc = Jsoup
+          .connect(url)
+          .userAgent(userAgent)
+          .header("Accept-Encoding", "gzip, deflate")
+          .header("Accept", "*/*")
+          .get();
+
+      Elements elements = doc.select("script[type=application/ld+json]");
+
+      for (Element element : elements) {
+        JsonObject jsonData = new Gson().fromJson(element.html(), JsonObject.class);
+        if (!jsonData.has("offers")) continue;
+
+        return jsonData.getAsJsonObject("offers").get("price").getAsString();
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    return "0";
+  }
+}
