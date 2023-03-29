88ef1f9187d1ff8a50c1c9c8deb9fa310cde2595
mListek <michal.listex@gmail.com>
2023-03-29 12:12:34 +0200

    moved links to a separate file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,43 +5,83 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
   public static void main(String[] args) {
 
-    final List<String> URLS = new ArrayList<>(List.of(
-        "https://www.euro.com.pl/konsole-nintendo-switch/nintendo-switch-lite-niebieski.bhtml",
-        "https://mediamarkt.pl/konsole-i-gry/konsola-nintendo-switch-lite-turkusowa",
-        "https://www.x-kom.pl/p/649842-konsola-nintendo-nintendo-switch-lite-niebieski.html",
-        "https://www.morele.net/nintendo-switch-lite-blue-5947533/",
-        "https://www.amazon.pl/Konsola-Nintendo-Switch-kolorze-niebieskim/dp/B092JL5NS2/",
-        "https://www.neonet.pl/konsole-i-gry/nintendo-switch-lite-grey.html"
-    ));
-
+    final List<String> URLS = new ArrayList<>();
     final List<String> USER_AGENTS = new ArrayList<>(List.of(
         "python-requests/2.28.2",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
     ));
 
-    System.out.println("Nintendo Switch Lite:");
+    // get all the links from a file
+    try (Scanner scan =
+             new Scanner(new File("src/main/resources/data.txt"))) {
+      while(scan.hasNext()) {
+        URLS.add(scan.nextLine());
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
 
-    String euroPrice = getPriceJson(URLS.get(0), USER_AGENTS.get(0));
-    System.out.println("euro.com.pl -> " + euroPrice + " zł");
+    System.out.println("Nintendo Switch Lite prices:");
 
-    String mediaMarktPrice = getPriceJson(URLS.get(1), USER_AGENTS.get(1));
-    System.out.println("mediamarkt.pl -> " + mediaMarktPrice + " zł");
+    List<PriceJson> pricesJson = Collections.synchronizedList(new ArrayList<>());
+    ExecutorService executorService = Executors.newFixedThreadPool(4);
 
-    String xkomPrice = getPriceJson(URLS.get(2), USER_AGENTS.get(1));
-    System.out.println("x-kom.pl -> " + xkomPrice + " zł");
+    for (String url : URLS) {
+      if (url.contains("mediamarkt.pl") || url.contains("x-kom.pl")) {
+        PriceJson priceJson = new PriceJson(url, USER_AGENTS.get(1));
+        pricesJson.add(priceJson);
+      } else if (url.contains("euro.com.pl")) {
+        PriceJson priceJson = new PriceJson(url, USER_AGENTS.get(0));
+        pricesJson.add(priceJson);
+      } else if (url.contains("morele.net")) {
 
-    String morelePrice = getPriceMorele(URLS.get(3));
-    System.out.println("morele.net -> " + morelePrice + " zł");
+      } else if (url.contains("amazon.pl")) {
 
-    String amazonPrice = getPriceAmazon(URLS.get(4));
-    System.out.println("amazon.pl -> " + amazonPrice + " zł");
+      }
+    }
+
+    for (PriceJson priceJson : pricesJson) {
+      Future<String> result = executorService.submit(priceJson);
+      try {
+        System.out.println("Future result euro.com.pl: " + result.get() + " zł");
+
+      } catch (InterruptedException | ExecutionException e) {
+        e.printStackTrace();
+      }
+    }
+
+
+    executorService.shutdown();
+
+//    String euroPrice = getPriceJson(URLS.get(0), USER_AGENTS.get(0));
+//    System.out.println("euro.com.pl -> " + euroPrice + " zł");
+//
+//    String mediaMarktPrice = getPriceJson(URLS.get(1), USER_AGENTS.get(1));
+//    System.out.println("mediamarkt.pl -> " + mediaMarktPrice + " zł");
+//
+//    String xkomPrice = getPriceJson(URLS.get(2), USER_AGENTS.get(1));
+//    System.out.println("x-kom.pl -> " + xkomPrice + " zł");
+//
+//    String morelePrice = getPriceMorele(URLS.get(3));
+//    System.out.println("morele.net -> " + morelePrice + " zł");
+//
+//    String amazonPrice = getPriceAmazon(URLS.get(4));
+//    System.out.println("amazon.pl -> " + amazonPrice + " zł");
   }
 
   public static String getPriceJson(String url, String userAgent) {
